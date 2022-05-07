2bc6226bc695642e3cb666395d0ffde288a31deb
Sarahi13Rivera <99987448+Sarahi13Rivera@users.noreply.github.com>
2022-05-07 15:24:10 -0600

    Add files via upload

---
+++ EVA2_14_STATIC/src/eva2_14_static/EVA2_14_STATIC.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_14_static;
+
+/**
+ *
+ * @author HP1000
+ */
+public class EVA2_14_STATIC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+    }
+}
