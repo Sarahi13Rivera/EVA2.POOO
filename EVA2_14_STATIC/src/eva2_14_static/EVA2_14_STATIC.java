578052bfbb49891f61c89a76fca6238d6cccdb21
Sarahi13Rivera <99987448+Sarahi13Rivera@users.noreply.github.com>
2022-05-08 16:45:09 -0600

    Update EVA2_14_STATIC.java

--- EVA2_14_STATIC/src/eva2_14_static/EVA2_14_STATIC.java
+++ EVA2_14_STATIC/src/eva2_14_static/EVA2_14_STATIC.java
@@ -16,12 +16,49 @@ public class EVA2_14_STATIC {
      */
     public static void main(String[] args) {
 
-
-
-
-
-
-
-
     }
+}
+class EstudiantePosgrado extends Estudiante{
+    
+}
+final class Estudiante extends Persona{
+    private String numeroControl;
+    
+    public Estudiante(){
+    }
+
+    public Estudiante(String numeroControl) {
+        this.numeroControl = numeroControl;
+    }
+     public Estudiante(String numeroControl, String nombre) {
+        super(nombre);
+         this.numeroControl = numeroControl;
+    }
+
+    public String getNumeroControl() {
+        return numeroControl;
+    }
+
+    public void setNumeroControl(String numeroControl) {
+        this.numeroControl = numeroControl;
+    }
+    
+}
+class Persona{
+private String nombre;
+    public Persona() {
+    }
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
 }
